96c014b8f3720b5d73ee262c0e2bf6061c635919
Spill-Tea <delrioj17@gmail.com>
2022-04-14 22:28:26 -0700

    Add unit testing suites.

---
+++ tests/test_query.py
@@ -0,0 +1,42 @@
+"""
+    AirtablePy/test_query.py
+
+"""
+# Python Dependencies
+import pytest
+
+from AirtablePy import query
+
+
+# Global Variable
+FAILURE = pytest.mark.xfail(raises=ValueError)
+
+
+@pytest.mark.parametrize("value, expected", [
+    ("NOW()", "NOW()"),
+    ("date", "'date'"),
+    ("CREATED_TIME()", "CREATED_TIME()")
+])
+def test_formula(value, expected):
+    assert query.is_formula(value) == expected
+
+
+@pytest.mark.parametrize("value, expected", [
+    ("CREATED_TIME()", "CREATED_TIME()"),
+    ("DATETIME_PARSE(val1, val2)", "DATETIME_PARSE(val1, val2)"),
+    ("RECORD_ID()", "RECORD_ID()"),
+    ("date", "{date}"),
+])
+def test_column(value, expected):
+    assert query.is_column(value) == expected
+
+
+@pytest.mark.parametrize("col, start, end, comp, expected", [
+    pytest.param(None, "20220814", "20220817", "day", "", marks=FAILURE),
+    pytest.param("column", None, None, "day", "", marks=FAILURE),
+    ("date", "20220814", None, "day", "OR(IS_AFTER({date}, '20220814'), IS_SAME({date}, '20220814', 'day'))"),
+    ("date", None, "20220817", "day", "OR(IS_BEFORE({date}, '20220817'), IS_SAME({date}, '20220817', 'day'))"),
+    ("date", "20220814", "20220817", "day", "AND(OR(IS_AFTER({date}, '20220814'), IS_SAME({date}, '20220814', 'day')), OR(IS_BEFORE({date}, '20220817'), IS_SAME({date}, '20220817', 'day')))"),
+])
+def test_date_query(col, start, end, comp, expected):
+    assert query.date_query(col, start, end, comp) == expected
---
+++ tests/test_utils.py
@@ -0,0 +1,44 @@
+"""
+    AirtablePy/test_utils.py
+
+"""
+# Python Dependencies
+import pytest
+from random import choices
+from string import ascii_letters
+
+from AirtablePy import utils
+
+# Global Variable
+FAILURE = pytest.mark.xfail(raises=ValueError)
+
+
+def random_key(length: int) -> str:
+    return "".join(choices(ascii_letters, k=length))
+
+
+@pytest.mark.parametrize("key, n, _type", [
+    ("app", 14, "Base ID"),
+    ("key", 14, "API Key"),
+    ("rec", 14, "Record ID"),
+    pytest.param("app", 13, "Base ID", marks=FAILURE),
+    pytest.param("app", 15, "Base ID", marks=FAILURE),
+    pytest.param("xxx", 14, "Base ID", marks=FAILURE),
+    pytest.param("key", 13, "API Key", marks=FAILURE),
+    pytest.param("key", 17, "API Key", marks=FAILURE),
+    pytest.param("tea", 14, "API Key", marks=FAILURE),
+    pytest.param("rec", 13, "Record ID", marks=FAILURE),
+    pytest.param("rec", 18, "Record ID", marks=FAILURE),
+    pytest.param("", 17, "Invalid", marks=FAILURE),
+    pytest.param("", 17, "Test", marks=FAILURE),
+])
+def test_check_key(key, n, _type):
+    assert utils.check_key(f"{key}{random_key(n)}", _type) is None
+
+
+@pytest.mark.parametrize("data, typecast, expected", [
+    ([{"A": 1, "B": 2}], True, {"records": [{"fields": {"A": 1, "B": 2}}], "typecast": True}),
+    ([{"A": 1, "B": 2}], False, {"records": [{"fields": {"A": 1, "B": 2}}], "typecast": False}),
+])
+def test_construct_record(data, typecast, expected):
+    assert utils.construct_record(data, typecast) == expected
