1ba63398e6ad2fbfe534f6d92ee8bd4a1100f056
Spill-Tea <delrioj17@gmail.com>
2022-04-18 22:29:27 -0700

    Reduce line length of unit test.

--- tests/test_query.py
+++ tests/test_query.py
@@ -32,12 +32,16 @@ def test_column(value, expected):
     assert query.is_column(value) == expected
 
 
+date_result = "AND(OR(IS_AFTER({date}, '20220814'), IS_SAME({date}, '20220814', 'day')), "
+date_result += "OR(IS_BEFORE({date}, '20220817'), IS_SAME({date}, '20220817', 'day')))"
+
+
 @pytest.mark.parametrize("col, start, end, comp, expected", [
     pytest.param(None, "20220814", "20220817", "day", "", marks=FAILURE),
     pytest.param("column", None, None, "day", "", marks=FAILURE),
     ("date", "20220814", None, "day", "OR(IS_AFTER({date}, '20220814'), IS_SAME({date}, '20220814', 'day'))"),
     ("date", None, "20220817", "day", "OR(IS_BEFORE({date}, '20220817'), IS_SAME({date}, '20220817', 'day'))"),
-    ("date", "20220814", "20220817", "day", "AND(OR(IS_AFTER({date}, '20220814'), IS_SAME({date}, '20220814', 'day')), OR(IS_BEFORE({date}, '20220817'), IS_SAME({date}, '20220817', 'day')))"),
+    ("date", "20220814", "20220817", "day", date_result),
 ])
 def test_date_query(col, start, end, comp, expected):
     assert query.date_query(col, start, end, comp) == expected
